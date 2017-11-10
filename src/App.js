4a38dae484fda8d9da18c4502047b0509f761576
benoit delamaire <benoit@cogitstudio.com>
2017-11-10 15:28:09 +0100

    init + test

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+import Nav from "components/nav";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Nav> coucou </Nav>
+      </div>
+    );
+  }
+}
+
+export default App;
