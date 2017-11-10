f9e5ae18fe8710a2df56f9ab71ece91b5eff6fca
benoit delamaire <benoit@cogitstudio.com>
2017-11-10 19:31:07 +0100

    premier compo, un bout de nav

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 
 import Nav from "components/nav";
 
 class App extends Component {
+  navItems = () => [
+    <Link key={'randomId222'} to="/">lien 1</Link>,
+    <Link key={'randomId2SDAD'} to="/2">lien 2</Link>,
+    <Link key={'∆Ω∆'} to="/3">lien 3</Link>
+  ];
   render() {
     return (
-      <div className="App">
-        <Nav> coucou </Nav>
-      </div>
+      <Router>
+        <div className="App">
+          <Nav buttonLabel={"nav"}>{this.navItems()}</Nav>
+        </div>
+      </Router>
     );
   }
 }
