061fe52de1b64310d1b8f82c2f4215845f5669b2
Fabien Le Frapper <contact@fabienlefrapper.me>
2017-12-01 14:39:23 +0100

    Start to draw a sinusoide

--- src/components/Wave/index.js
+++ src/components/Wave/index.js
@@ -1,5 +1,20 @@
 import React, { Component } from 'react'
 
-const Wave = () => <h1>COUCOU</h1>
+export default class Wave extends Component {
+  componentDidMount() {
+    const c = document.getElementById('wave')
+    const ctx = c.getContext('2d')
 
-export default Wave
+    for (let x = 0; x <= 360; x += 1) {
+      const y = 180.0 - Math.sin(x * Math.PI / 180) * 120
+      ctx.lineTo(x, y)
+    }
+
+    ctx.scale(1, 1)
+    ctx.stroke()
+  }
+
+  render() {
+    return <canvas id="wave" />
+  }
+}
